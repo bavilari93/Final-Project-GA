d973322f679d03d138fd42706ee3ab590d8ab558
Bianca A <frank@Bianca-Avila-4.local>
2017-08-09 21:31:26 -0400

    user auth

--- apetit/db/seed.sql
+++ apetit/db/seed.sql
@@ -2,16 +2,20 @@ DROP TABLE  IF EXISTS restaurants CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users(
-id SERIAL PRIMARY KEY, 
+id BIGSERIAL PRIMARY KEY,
+name VARCHAR NOT NULL, 
 email VARCHAR NOT NULL UNIQUE,
 location VARCHAR NOT NULL,
-password VARCHAR NOT NULL
-)
+password_digest VARCHAR NOT NULL,
+token VARCHAR NOT NULL
+
+-- add token when doing auth
+);
 
 
 CREATE TABLE restaurants(
 id BIGSERIAL PRIMARY KEY, 
-user_id INT REFERENCES users(id),
+-- user_id INT REFERENCES users(id),
 name VARCHAR NOT NULL,
 location VARCHAR(255) NOT NULL,  
 latitude FLOAT NOT NULL,
@@ -25,9 +29,15 @@ aggregaterating VARCHAR NOT NULL
 );
 
 
+INSERT INTO users(name, email, location, password_digest, token)VALUES
+('bianca', 
+'bianca@gmail.com', 
+'new york', 
+'puky', 
+'daasddssd'
+);
 
-
-INSERT INTO restaurants (name, location, latitude, longitude, averagecost, pricerange, thunmpic, cusines, ratingcolor, aggregaterating)VALUES 
+INSERT INTO restaurants (name, location, latitude, longitude, averagecost, pricerange, thunmpic, cuisines, ratingcolor, aggregaterating)VALUES 
 (
 	'el restaurante','el sitio',
 	 3000.000, 
--- apetit/index.js
+++ apetit/index.js
@@ -4,6 +4,8 @@ const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 8000
 const cors = require('cors');
+const Auth = require('./services/auth')
+const logger = require('morgan');
 
 
 // cross origin request 
@@ -18,6 +20,17 @@ app.use(express.static(__dirname + '/public'));
 // body parser 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
+
+// logger to see whats going on
+app.use(logger('dev'));
+
+// before all routes, use the middleware we define in Auth to get the
+// current user, or i can put it directy when requiring the controller 
+app.use(Auth.authenticate);
+// routes for log in and log out 
+// set up base routes
+app.use('/users', require('./controllers/users'));
+app.use('/login', require('./controllers/sessions'));
 
 
 app.use('/api', require('./controllers/restaurant.js'));
